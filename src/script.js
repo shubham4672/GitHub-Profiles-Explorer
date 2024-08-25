19fc93a93de3d1795a0a2634e534d5f5c5a96121
shubham4672 <shubham4672.20@gmail.com>
2024-08-25 18:43:43 +0530

    edge cases handle

--- src/script.js
+++ src/script.js
@@ -16,26 +16,34 @@ button.addEventListener("click", () => {
 const apiUrl = "https://api.github.com/users/";
 
 function displayDetails(user) {
+  const userName = user.name ? `<h1>${user.name}</h1>` : "";
+  const userBio = user.bio ? `<p>${user.bio}</p>` : "";
   const cardHTML = `
   <div class="card">
         <div class="img" style="background-color: black;border-radius: 243px">
           <img src=${user.avatar_url} alt="${user.name}" style="border-radius: 243px;" />
         </div>
         <div class="info">
-          <h1>${user.name}</h1>
-          <p>
-            ${user.bio}
-          </p>
+          ${userName}
+          ${userBio}
           <ul>
             <li><strong>${user.followers} Followers</strong></li>
             <li><strong>${user.following} Following</strong></li>
             <li><strong>${user.public_repos} Repositories</strong></li>
           </ul>
-          <div class="repo"></div>
+          <div class="repo">
+            <a href="#">Time</a>
+          </div>
         </div>
       </div>
     `;
   main.innerHTML = cardHTML;
+}
+
+function notFound() {
+  main.style.display = "none";
+  error.style.display = "block";
+  error.innerHTML = "User name not found";
 }
 
 async function getUser(username) {
@@ -43,13 +51,33 @@ async function getUser(username) {
     const { data } = await axios(apiUrl + username);
     error.style.display = "none";
     main.style.display = "block";
-    console.log(data);
     displayDetails(data);
+    getRepos(data);
   } catch (err) {
-    main.style.display = "none";
-    error.style.display = "block";
-    error.innerHTML = "User name not found";
+    notFound();
   }
+}
+
+async function getRepos(username) {
+  try {
+    const { data } = await axios(apiUrl + username + "/repos?sort=created");
+    addReposToCard(data);
+  } catch (err) {
+    notFound();
+  }
+}
+
+function addReposToCard(repos) {
+  const reposEl = document.querySelector(".repo");
+  repos.forEach((repo) => {
+    const repoEl = document.createElement("a");
+    repoEl.classList.add("repo");
+    repoEl.href = repo.html_url;
+    repoEl.target = "_blank";
+    repoEl.innerText = repo.name;
+
+    reposEl.appendChild(repoEl);
+  });
 }
 
 form.addEventListener("submit", (e) => {
@@ -57,6 +85,7 @@ form.addEventListener("submit", (e) => {
   const user = input.value;
   if (user) {
     getUser(input.value);
+
     input.value = "";
   }
 });
