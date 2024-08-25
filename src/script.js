f0ad558f5b0889c79e20ca746b9dd4aef2c2221c
shubham4672 <shubham4672.20@gmail.com>
2024-08-25 13:55:23 +0530

    github

---
+++ src/script.js
@@ -0,0 +1,65 @@
+"use strict";
+
+const input = document.querySelector(".input");
+const form = document.querySelector(".form");
+const main = document.querySelector(".main");
+const error = document.querySelector(".error");
+const button = document.querySelector(".button");
+const bg = document.querySelector(".bg");
+
+button.addEventListener("click", () => {
+  bg.classList.toggle("light");
+  bg.classList.toggle("dark");
+});
+
+// API base URL
+const apiUrl = "https://api.github.com/users/";
+
+function displayDetails(user) {
+  const cardHTML = `
+  <div class="card">
+        <div class="img" style="background-color: black;border-radius: 23">
+          <img src=${user.avatar_url} alt="${user.name}" style="border-radius: 253px;" />
+        </div>
+        <div class="info">
+          <h1>${user.name}</h1>
+          <p>
+            ${user.bio}
+          </p>
+          <ul>
+            <li><strong>${user.followers} Followers</strong></li>
+            <li><strong>${user.following} Following</strong></li>
+            <li><strong>${user.public_repos} Repositories</strong></li>
+          </ul>
+          <div class="repo">
+            <a href="#">Rep1</a>
+            <a href="#">Rep2</a>
+            <a href="#">Rep3</a>
+            <a href="#">Rep4</a>
+          </div>
+        </div>
+      </div>
+    `;
+  main.innerHTML = cardHTML;
+}
+
+async function getUser(username) {
+  try {
+    const { data } = await axios(apiUrl + username);
+    error.style.display = "none";
+    main.style.display = "block";
+    console.log(data);
+    displayDetails(data);
+  } catch (err) {
+    if (username !== "") {
+      main.style.display = "none";
+      error.style.display = "block";
+      error.innerHTML = "User name not found";
+    }
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getUser(input.value);
+});
